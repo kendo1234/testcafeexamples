35efd82acbfaa00df1d12c9dadf4f1f9265f7102
kendo1234 <kendomustdie@googlemail.com>
2022-03-02 20:07:52 +0000

    messing around with config

--- tests/basicActions.js
+++ tests/basicActions.js
@@ -7,7 +7,6 @@ test('Do a search', async t => {
     await t
         .typeText('#gh-ac', 'Batarangs')
         .click('#gh-btn')
-
-        .expect(Selector('#mainContent > div.s-answer-region.s-answer-region-center-top > div > div.clearfix.srp-controls__row-2 > div:nth-child(1) > div.srp-controls__control.srp-controls__count > h1 > span:nth-child(2)').innerText).eql('batarangs');
+        .expect(Selector('#s0-1').innerText).eql('batarangs: Search Result | eBay');
 
 });
